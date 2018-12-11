56ca2e8059fff1c0f19067dd5fffb77ba92847c9
Daniel Kessler <kesslerd@umich.edu>
2018-12-11 17:19:44 -0500

    hooray

--- oglasso/test.py
+++ oglasso/test.py
@@ -25,13 +25,13 @@ grps = [[i] for i in range(n)]
 
 A = np.random.randn(m,n)
 xstar = np.zeros((n,1))
-xstar[[0,1,2]] = 100
+xstar[[0,1,2]] = 10
 b = A.dot(xstar) + np.random.randn(m,1)
 
 x0 = np.zeros((n,1))
-o1 = og.ladmm(x0,A,b,lam=10,niter=1000,r=1)
+o1 = og.ladmm(x0,A,b,lam=100,niter=100,r=1)
 o1[0]
-o2 = og.admm(x0, A, b, grps, lam=10, niter = 1000, r=1)
+o2 = og.admm(x0, A, b, grps, lam=100, niter = 100, r=1)
 o2[1][1]
 print('hi')
 
