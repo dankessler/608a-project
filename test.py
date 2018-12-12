03957484e255c5695bf4b5ac8c29528a85a6b57e
Daniel Kessler <kesslerd@umich.edu>
2018-12-11 19:55:59 -0500

    move test script

--- test.py
+++ test.py
@@ -35,7 +35,24 @@ o2 = og.admm(x0, A, b, grps, lam=100, niter = 100, r=1)
 o2[1][1]
 print('hi')
 
+# try new sim function
+m = 50
+n = 100
+G = [[i] for i in range(n)]
 
+xstar = np.zeros((n,1))
+xstar[0:25] = 10
+x0 = np.zeros((n,1))
+
+out = og.sim2test(m,n,G,xstar,x0,r=1,lam=1,eps=1e-1)
+out = og.sim2test(m,n,G,xstar,x0,r=1,lam=10,eps=1e-1)
+
+
+o1 = og.ladmm(x0,A,b,lam=100,niter=100,r=1)
+o1[0]
+o2 = og.admm(x0, A, b, grps, lam=100, niter = 100, r=1)
+o2[1][1]
+print('hi')
 
 
 
